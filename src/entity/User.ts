0708f92fe8b43f2d8e681de79c6c0f7fc39555a4
chenyitao <285345240@qq.com>
2020-06-07 18:52:49 +0800

    feat: 配置babel

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,18 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column} from 'typeorm';
 
 @Entity()
 export class User {
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    firstName: string;
+  @Column('text')
+  firstName: string;
 
-    @Column()
-    lastName: string;
+  @Column('text')
+  lastName: string;
 
-    @Column()
-    age: number;
+  @Column('int')
+  age: number;
 
 }
